b6190131e6c094e43b6fc2ddbe8d3675cc09c374
siberian-git <73994218+siberian-git@users.noreply.github.com>
2021-01-31 18:11:42 +0100

    2.00

--- versions/version.js
+++ versions/version.js
@@ -1,6 +1,6 @@
 {
 "curr_version" : "1.5c",
-"beta_version" : "1.99d",
+"beta_version" : "2.00",
 "curr_version_file" : "Emby_Xnoppo.v1.5c.zip",
-"beta_version_file" : "Emby_Xnoppo.v1.99d.zip"
+"beta_version_file" : "Emby_Xnoppo.v2.00.zip"
 }
